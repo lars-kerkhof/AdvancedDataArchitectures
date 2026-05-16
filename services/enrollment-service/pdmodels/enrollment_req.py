24e715cee50121e85f356a6b7c97cc91f4a548a0
lars-kerkhof <Larskerkhof03@gmail.com>
2026-05-16 17:13:07 +0200

    Update enrollment_req.py

--- services/enrollment-service/pdmodels/enrollment_req.py
+++ services/enrollment-service/pdmodels/enrollment_req.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
-
+from typing import List
 
 class EnrollmentReq(BaseModel):
     candidate_id: str
     trial_id: str
     match_score: int
-    match_reason: str
+    match_reasons: List[str]
